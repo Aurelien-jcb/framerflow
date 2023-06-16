c1028439825697583f689afed839b30ee88af5d9
ajacob <ajacob@atecna.fr>
2023-06-16 16:03:09 +0200

    check if metadata works inside layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@
 
 import "@/styles/globals.css"
 import { Fragment, useEffect, useState } from "react"
-// import { Metadata } from "next"
+import { Metadata } from "next"
 import { Transition } from "@headlessui/react"
 
 import { siteConfig } from "@/config/site"
@@ -13,22 +13,22 @@ import { TopBar } from "@/components/layout/top-bar"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
 
-// export const metadata: Metadata = {
-//   title: {
-//     default: siteConfig.name,
-//     template: `%s - ${siteConfig.name}`,
-//   },
-//   description: siteConfig.description,
-//   themeColor: [
-//     { media: "(prefers-color-scheme: light)", color: "white" },
-//     { media: "(prefers-color-scheme: dark)", color: "black" },
-//   ],
-//   icons: {
-//     icon: "/favicon.ico",
-//     shortcut: "/favicon-16x16.png",
-//     apple: "/apple-touch-icon.png",
-//   },
-// }
+export const metadata: Metadata = {
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ]
+  // icons: {
+  //   icon: "/favicon.ico",
+  //   shortcut: "/favicon-16x16.png",
+  //   apple: "/apple-touch-icon.png",
+  // },
+}
 
 interface RootLayoutProps {
   children: React.ReactNode
